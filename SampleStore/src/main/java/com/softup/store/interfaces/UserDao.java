3e9580d4245e922821c88a985df8c1bf137a22e7
vahidsaneei <vsdesire@gmail.com>
2019-03-07 23:30:02 +0330

    UserDao interface
    userdao is responsible to interaction with DB in users issues.

--- SampleStore/src/main/java/com/softup/store/interfaces/UserDao.java
+++ SampleStore/src/main/java/com/softup/store/interfaces/UserDao.java
@@ -1,5 +1,15 @@
 package com.softup.store.interfaces;
 
+import java.util.List;
+
+import com.softup.store.models.User;
+
 public interface UserDao {
+
+	String addUser(User user);
+
+	User findByUsername(String username);
+
+	List<User> getAllUsers();
 
 }
