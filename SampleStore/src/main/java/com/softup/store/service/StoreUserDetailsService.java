b9c944e88181f792bc3ee761617413c80067494f
vahidsaneei <vsdesire@gmail.com>
2019-03-07 23:37:18 +0330

    UserDetailsService
    implementing a class implementing UserDetailsService interface that
    spring security can do authentication with db by using this Calss.

---
+++ SampleStore/src/main/java/com/softup/store/service/StoreUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.softup.store.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.softup.store.interfaces.UserDao;
+
+@Transactional(readOnly = true)
+@Service("myUserDetailsService")
+public class StoreUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+
+		com.softup.store.models.User user = userDao.findByUsername(username);
+
+		return new User(user.getUsername(), user.getPassword(), user.isEnabled(), user.isAccountNonExpired(),
+				user.isCredentialsNonExpired(), user.isAccountNonLocked(), user.getAuthorities());
+
+	}
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+}
