8d26f398b0c7d0ba2e694f98762552f8d0268402
vahidsaneei <vsdesire@gmail.com>
2019-03-07 23:31:43 +0330

    UserServiceImpl
    implementing user service from userService interface. we have a userDao
    to interact with database.

---
+++ SampleStore/src/main/java/com/softup/store/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.softup.store.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.softup.store.interfaces.UserDao;
+import com.softup.store.interfaces.UserService;
+import com.softup.store.models.User;
+
+@Service(value = "userService")
+@Transactional
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserDao userDao;
+
+	@Transactional
+	public String addUser(User user) {
+		return userDao.addUser(user);
+	}
+
+	@Transactional
+	public User findByUsername(String username) {
+		return userDao.findByUsername(username);
+	}
+
+	@Transactional
+	public List<User> getAllUsers() {
+		return userDao.getAllUsers();
+	}
+
+}
