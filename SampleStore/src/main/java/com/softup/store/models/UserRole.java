1bfeb481113f464d76edfdfacefd369f9cde1c80
vahidsaneei <vsdesire@gmail.com>
2019-03-07 22:51:38 +0330

    some change in user inserted for check credential in later. and create userrole class that set,create,return role for any user.

---
+++ SampleStore/src/main/java/com/softup/store/models/UserRole.java
@@ -0,0 +1,63 @@
+package com.softup.store.models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.softup.store.models.User;
+
+@Entity
+@Table(name = "user_roles", uniqueConstraints = @UniqueConstraint(columnNames = { "username", "role" }))
+public class UserRole {
+
+	private Integer userRoleId;
+	private User user;
+	private String role;
+
+	public UserRole() {
+	}
+
+	public UserRole(User user, String role) {
+		this.user = user;
+		this.role = role;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_role_id", unique = true, nullable = false)
+	public Integer getUserRoleId() {
+		return this.userRoleId;
+	}
+
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "username", nullable = false)
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Column(name = "role", nullable = false, length = 45)
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
