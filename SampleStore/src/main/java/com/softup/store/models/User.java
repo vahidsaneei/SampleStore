215066834e76b16b6cb22314f448a8c4a745641d
vahidsaneei <vsdesire@gmail.com>
2019-03-07 22:41:29 +0330

    add user class at first.so, we can create user service and login pattern

---
+++ SampleStore/src/main/java/com/softup/store/models/User.java
@@ -0,0 +1,9 @@
+package com.softup.store.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -5599532149952411879L;
+
+}
