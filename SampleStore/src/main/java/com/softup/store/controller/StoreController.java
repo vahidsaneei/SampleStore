9ff410bf9809cba0af733c19033a1095950bb642
vahidsaneei <vsdesire@gmail.com>
2019-03-07 22:58:02 +0330

    StoreController
    main controller that handle login and logout and registering users.

---
+++ SampleStore/src/main/java/com/softup/store/controller/StoreController.java
@@ -0,0 +1,10 @@
+package com.softup.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+public class StoreController {
+
+}
