69d4e0ca4799189be256ac467fa8c74ccab8bbf8
vahidsaneei <vsdesire@gmail.com>
2019-03-07 23:30:45 +0330

    UserService Interface
    is the interface between user and system.

--- SampleStore/src/main/java/com/softup/store/interfaces/UserService.java
+++ SampleStore/src/main/java/com/softup/store/interfaces/UserService.java
@@ -1,5 +1,16 @@
 package com.softup.store.interfaces;
 
+import java.util.List;
+
+import com.softup.store.models.User;
+
+
 public interface UserService {
+
+	String addUser(User user);
+
+	User findByUsername(String username);
+
+	List<User> getAllUsers();
 
 }
