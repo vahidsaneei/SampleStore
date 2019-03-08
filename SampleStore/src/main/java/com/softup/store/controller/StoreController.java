fc32c20d8d908743332ba5639185ed44ade76684
Ali <Ali@Micro-Roni2>
2019-03-08 14:37:07 +0330

    StoreController
    create controller that dispatcher some requests that have relation to
    Store by checking request uri.

--- SampleStore/src/main/java/com/softup/store/controller/StoreController.java
+++ SampleStore/src/main/java/com/softup/store/controller/StoreController.java
@@ -1,10 +1,30 @@
 package com.softup.store.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.softup.store.interfaces.ProductService;
+import com.softup.store.models.Product;
 
 @Controller
 @EnableWebMvc
 public class StoreController {
+
+	@Autowired
+	ProductService productService;
+
+	@RequestMapping(value = { "/", "index", "home" }, method = RequestMethod.GET)
+	public ModelAndView gethome() {
+		ModelAndView model = new ModelAndView("welcomepage");
+		List<Product> products = productService.getAllProducts();
+		model.addObject("products", products);
+		return model;
+	}
 
 }
