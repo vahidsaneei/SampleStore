5e81c27033ca11f63f1ab7e9132116f1f31c50de
vahidsaneei <vsdesire@gmail.com>
2019-03-11 01:08:24 +0330

    Product class
    price field alter from double to bigdecimal

--- SampleStore/src/main/java/com/softup/store/models/Product.java
+++ SampleStore/src/main/java/com/softup/store/models/Product.java
@@ -1,6 +1,7 @@
 package com.softup.store.models;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -56,7 +57,7 @@ public class Product implements Serializable {
 	private Integer quantity;
 
 	@Column(name = "price", nullable = false)
-	private double price;
+	private BigDecimal price;
 
 	@Column(name = "category", nullable = false)
 	private String category;
@@ -67,7 +68,7 @@ public class Product implements Serializable {
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "products")
 	private List<Order> orders = new ArrayList<Order>();
 
-	public Product(String fullName, String companyName, Date expiryDate, Integer quantity, double price,
+	public Product(String fullName, String companyName, Date expiryDate, Integer quantity, BigDecimal price,
 			String description) {
 		super();
 		this.fullName = fullName;
@@ -78,7 +79,7 @@ public class Product implements Serializable {
 		this.description = description;
 	}
 
-	public Product(String fullName, String companyName, Integer quantity, Date expiryDate, double price,
+	public Product(String fullName, String companyName, Integer quantity, Date expiryDate, BigDecimal price,
 			String category) {
 		super();
 		this.fullName = fullName;
@@ -168,11 +169,11 @@ public class Product implements Serializable {
 		this.expiryDate = expiryDate;
 	}
 
-	public double getPrice() {
+	public BigDecimal getPrice() {
 		return price;
 	}
 
-	public void setPrice(double price) {
+	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 
@@ -216,9 +217,19 @@ public class Product implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((fullName == null) ? 0 : seller.hashCode());
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
 		result = prime * result + ((companyName == null) ? 0 : companyName.hashCode());
+		result = prime * result + ((createdDate == null) ? 0 : createdDate.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((expiryDate == null) ? 0 : expiryDate.hashCode());
+		result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((insertDate == null) ? 0 : insertDate.hashCode());
+		result = prime * result + ((likeCount == null) ? 0 : likeCount.hashCode());
+		result = prime * result + ((orders == null) ? 0 : orders.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+		result = prime * result + ((seller == null) ? 0 : seller.hashCode());
 		return result;
 	}
 
@@ -231,20 +242,30 @@ public class Product implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
-		if (seller == null) {
-			if (other.seller != null)
+		if (category == null) {
+			if (other.category != null)
 				return false;
-		} else if (!seller.equals(other.seller))
+		} else if (!category.equals(other.category))
 			return false;
 		if (companyName == null) {
 			if (other.companyName != null)
 				return false;
 		} else if (!companyName.equals(other.companyName))
 			return false;
+		if (createdDate == null) {
+			if (other.createdDate != null)
+				return false;
+		} else if (!createdDate.equals(other.createdDate))
+			return false;
 		if (description == null) {
 			if (other.description != null)
 				return false;
 		} else if (!description.equals(other.description))
+			return false;
+		if (expiryDate == null) {
+			if (other.expiryDate != null)
+				return false;
+		} else if (!expiryDate.equals(other.expiryDate))
 			return false;
 		if (fullName == null) {
 			if (other.fullName != null)
@@ -256,7 +277,35 @@ public class Product implements Serializable {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+		if (insertDate == null) {
+			if (other.insertDate != null)
+				return false;
+		} else if (!insertDate.equals(other.insertDate))
+			return false;
+		if (likeCount == null) {
+			if (other.likeCount != null)
+				return false;
+		} else if (!likeCount.equals(other.likeCount))
+			return false;
+		if (orders == null) {
+			if (other.orders != null)
+				return false;
+		} else if (!orders.equals(other.orders))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
+			return false;
+		if (seller == null) {
+			if (other.seller != null)
+				return false;
+		} else if (!seller.equals(other.seller))
 			return false;
 		return true;
 	}
