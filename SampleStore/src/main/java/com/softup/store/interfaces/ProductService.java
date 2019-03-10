28d51dff2712aa40531a8f7dc132fb020285ee39
vahidsaneei <vsdesire@gmail.com>
2019-03-11 01:05:44 +0330

    product service
    service and dao define the ways that admin can interact on products

--- SampleStore/src/main/java/com/softup/store/interfaces/ProductService.java
+++ SampleStore/src/main/java/com/softup/store/interfaces/ProductService.java
@@ -1,5 +1,6 @@
 package com.softup.store.interfaces;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.softup.store.models.Product;
@@ -10,6 +11,28 @@ public interface ProductService {
 
 	Product findById(Long id);
 
+	String addProduct(Product product);
+
+	String removeProduct(Long id);
+
 	List<Product> findByName(String name);
+
+	List<Product> getNewestProducts();
+
+	List<Product> getPopularProducts();
+
+	List<Product> limitPrice(BigDecimal start, BigDecimal end);
+
+	List<Product> expireInNextDay(Integer num);
+
+	List<Product> getProductFromStore(String storename);
+
+	List<Product> mostCommentedProduct();
+
+	List<Product> mostOrdered();
+
+	void rechargeProduct(Product product, Integer quantity);
+
+	List<Product> emptyProducts();
 
 }
