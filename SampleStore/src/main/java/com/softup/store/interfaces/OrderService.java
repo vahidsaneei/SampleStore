c09a3393378624a22bbacbad7b7ba5c8b49ea73f
vahidsaneei <vsdesire@gmail.com>
2019-03-12 10:19:58 +0330

    Order Service
    create two interface to manage orders operations

---
+++ SampleStore/src/main/java/com/softup/store/interfaces/OrderService.java
@@ -0,0 +1,33 @@
+package com.softup.store.interfaces;
+
+import java.util.Date;
+import java.util.List;
+
+import com.softup.store.models.Orders;
+import com.softup.store.models.Period;
+import com.softup.store.models.Product;
+
+public interface OrderService {
+	
+	String addOrders(Orders orders);
+	
+	String updateOrders(Orders orders);
+	
+	String removeOrders(Long id);
+	
+	Orders findById(Long id);
+	
+	List<Orders> findByDate(Date date);
+	
+	List<Orders> canceledOrders();
+	
+	List<Orders> completeOrders();
+	
+	String cancelOrder(Orders orders,String cause);
+	
+	List<Orders> findOrdersByProduct(Product product);
+	
+	List<Product> saleInPeriod(Period period,Product product);
+	
+
+}
