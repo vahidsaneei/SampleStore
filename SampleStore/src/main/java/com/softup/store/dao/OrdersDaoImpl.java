53c13ac9319e01c29938bd353e266335d628c84a
vahidsaneei <vsdesire@gmail.com>
2019-03-12 10:20:47 +0330

    Order Service impl
    implementing service and Dao to clear operating of these

---
+++ SampleStore/src/main/java/com/softup/store/dao/OrdersDaoImpl.java
@@ -0,0 +1,135 @@
+package com.softup.store.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.TemporalType;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.softup.store.interfaces.OrdersDao;
+import com.softup.store.models.Orders;
+import com.softup.store.models.Period;
+import com.softup.store.models.Product;
+
+@Repository("ordersDao")
+public class OrdersDaoImpl implements OrdersDao {
+
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	protected Session session() {
+		try {
+			return sessionFactory.getCurrentSession();
+		} catch (HibernateException e) {
+			return sessionFactory.openSession();
+		}
+	}
+
+	public Orders findById(Long id) {
+		return session().get(Orders.class, id);
+	}
+
+	public List<Orders> findByDate(Date date) {
+		List<Orders> orders = session().createQuery("from orders where delivery=:date", Orders.class)
+				.setParameter(0, date, TemporalType.TIMESTAMP).list();
+		return orders;
+	}
+
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public List<Orders> canceledOrders() {
+		String sql = "SELECT * FROM ORDERS WHERE canceled=:canceled";
+		List<Orders> orders = session().createQuery(sql).setBoolean("canceled", Boolean.TRUE).list();
+
+		return orders;
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public List<Orders> completeOrders() {
+		String sql = "SELECT * FROM ORDERS WHERE success=:success";
+		List<Orders> orders = session().createQuery(sql).setBoolean("success", Boolean.TRUE).list();
+		return orders;
+	}
+
+	public String cancelOrder(Orders orders, String cause) {
+		orders.setCancelCause(cause);
+		orders.setCanceled(true);
+
+		return updateOrders(orders);
+
+	}
+
+	public List<Orders> findOrdersByProduct(Product product) {
+		return null;
+	}
+
+	public List<Product> saleInPeriod(Period period, Product product) {
+		return null;
+	}
+
+	public String addOrders(Orders orders) {
+		String result = "";
+
+		try {
+			session().save(orders);
+			result = "success";
+			session().flush();
+
+		} catch (Exception e) {
+			if (e.getCause().getMessage().toLowerCase().contains("duplicate"))
+				result = "error this  order already was exist";
+			else
+				result = "error " + e.getCause().getMessage();
+		} finally {
+			session().clear();
+		}
+		return result;
+	}
+
+	public String updateOrders(Orders orders) {
+		String result = "";
+
+		try {
+			session().update(orders);
+			result = "success";
+			session().flush();
+
+		} catch (Exception e) {
+			result = "error " + e.getCause().getMessage();
+		} finally {
+			session().clear();
+		}
+		return result;
+	}
+
+	public String removeOrders(Long id) {
+		String result = "";
+
+		try {
+			Orders orders = findById(id);
+
+			if (orders != null) {
+				session().remove(orders);
+				result = "success";
+				session().flush();
+			} else {
+				result = "error :orders not found";
+			}
+
+		} catch (Exception e) {
+			result = "error " + e.getCause().getMessage();
+		} finally {
+			session().clear();
+		}
+		return result;
+	}
+
+}
---
+++ SampleStore/src/main/java/com/softup/store/service/OrdersServiceImpl.java
@@ -0,0 +1,77 @@
+package com.softup.store.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.softup.store.interfaces.OrderService;
+import com.softup.store.interfaces.OrdersDao;
+import com.softup.store.models.Orders;
+import com.softup.store.models.Period;
+import com.softup.store.models.Product;
+
+@Service("orderService")
+@Transactional
+public class OrdersServiceImpl implements OrderService {
+
+	@Autowired
+	OrdersDao ordersDao;
+
+	public void setOrdersDao(OrdersDao ordersDao) {
+		this.ordersDao = ordersDao;
+	}
+
+	@Transactional(readOnly = true)
+	public Orders findById(Long id) {
+		return ordersDao.findById(id);
+	}
+
+	@Transactional
+	public List<Orders> findByDate(Date date) {
+		return ordersDao.findByDate(date);
+	}
+
+	@Transactional
+	public List<Orders> canceledOrders() {
+		return ordersDao.canceledOrders();
+	}
+
+	@Transactional
+	public List<Orders> completeOrders() {
+		return ordersDao.completeOrders();
+	}
+
+	@Transactional
+	public String cancelOrder(Orders orders, String cause) {
+		return ordersDao.cancelOrder(orders, cause);
+	}
+
+	@Transactional
+	public List<Orders> findOrdersByProduct(Product product) {
+		return ordersDao.findOrdersByProduct(product);
+	}
+
+	@Transactional
+	public List<Product> saleInPeriod(Period period, Product product) {
+		return ordersDao.saleInPeriod(period, product);
+	}
+
+	@Transactional
+	public String addOrders(Orders orders) {
+		return ordersDao.addOrders(orders);
+	}
+
+	@Transactional
+	public String updateOrders(Orders orders) {
+		return ordersDao.updateOrders(orders);
+	}
+
+	@Transactional
+	public String removeOrders(Long id) {
+		return ordersDao.removeOrders(id);
+	}
+
+}
