286ac2c1f859fde2b9828389cf4d5327128ac400
Ali <Ali@Micro-Roni2>
2019-03-15 03:05:02 +0330

    User controller
    add new controller
    for user interactions

---
+++ SampleStore/src/main/java/com/softup/store/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.softup.store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.softup.store.interfaces.UserService;
+import com.softup.store.models.User;
+
+@Controller
+@EnableWebMvc
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(value = "user/showprofile", method = RequestMethod.GET)
+	public ModelAndView getUserProfile() {
+		ModelAndView model = new ModelAndView("profilepage");
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userService.findByUsername(name);
+		model.addObject("user", user);
+		return model;
+
+	}
+}
