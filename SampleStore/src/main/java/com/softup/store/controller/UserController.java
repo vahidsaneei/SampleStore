5247a379fd237bfee48fe071208253306314468d
Ali <Ali@Micro-Roni2>
2019-03-18 22:54:05 +0330

    UserController
    two method for edit and edit added

--- SampleStore/src/main/java/com/softup/store/controller/UserController.java
+++ SampleStore/src/main/java/com/softup/store/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.softup.store.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,5 +40,58 @@ public class UserController {
 		List<User> users = userService.getAllUsers(username);
 		model.addObject("userList", users);
 		return model;
+	}
+
+	@RequestMapping(value = "users/newuser")
+	public ModelAndView addNewUser() {
+		ModelAndView model = new ModelAndView("adminnewuser");
+		User user = new User();
+		model.addObject("user", user);
+		return model;
+	}
+
+	@RequestMapping(value = "users/edituser/{id}", method = RequestMethod.GET)
+	public ModelAndView getUpdateUser(@PathVariable("id") Long id) {
+		ModelAndView model = new ModelAndView("adminnewuser");
+		User user = userService.findById(id);
+		model.addObject("user", user);
+		return model;
+	}
+
+	@RequestMapping(value = "users/saveuser")
+	public ModelAndView saveNewUser(@ModelAttribute("user") User user) {
+		ModelAndView model = new ModelAndView();
+
+		String res = "";
+
+		if (user.getId() != null)
+			res = userService.addUser(user);
+		else
+			res = userService.updateUser(user);
+
+		if (!res.contains("error")) {
+			model.addObject("message", res);
+		} else {
+			model.addObject("error", res);
+			model.setViewName("adminnewuser");
+		}
+
+		return model;
+	}
+
+	@ModelAttribute("roleList")
+	public List<String> roleList() {
+		List<String> roles = new ArrayList<String>();
+
+		roles.add("ROLE_ADMIN");
+		roles.add("ROLE_DBA_ADMIN");
+		roles.add("ROLE_SALE_MANAGER");
+		roles.add("ROLE_STORE_MANAGER");
+		roles.add("ROLE_USER");
+		roles.add("ROLE_FINANACE_MANAGER");
+		roles.add("ROLE_CASHIER");
+		roles.add("ROLE_EMPLOYEE");
+
+		return roles;
 	}
 }
