416f1eca2efb92aa4c4a40b3969af5e729662fde
vahidsaneei <vsdesire@gmail.com>
2019-03-29 00:40:26 +0430

    like service and dao
    create new method to receive products that specific user liked before

--- SampleStore/src/main/java/com/softup/store/interfaces/LikeDao.java
+++ SampleStore/src/main/java/com/softup/store/interfaces/LikeDao.java
@@ -1,10 +1,13 @@
 package com.softup.store.interfaces;
 
 import com.softup.store.entity.Likes;
+import com.softup.store.entity.Product;
+import com.softup.store.entity.User;
 
 public interface LikeDao {
 	String setLike(Likes likes);
 
 	String unLike(Likes like);
 
+	boolean likedByUser(User user, Product product);
 }
--- SampleStore/src/main/java/com/softup/store/interfaces/LikeService.java
+++ SampleStore/src/main/java/com/softup/store/interfaces/LikeService.java
@@ -1,11 +1,15 @@
 package com.softup.store.interfaces;
 
 import com.softup.store.entity.Likes;
+import com.softup.store.entity.Product;
+import com.softup.store.entity.User;
 
 public interface LikeService {
-	
+
 	String setLike(Likes likes);
-	
+
 	String unLike(Likes like);
+
+	boolean likedByUser(User user, Product product);
 
 }
