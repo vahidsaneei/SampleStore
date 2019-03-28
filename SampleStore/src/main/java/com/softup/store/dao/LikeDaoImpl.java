48a107c5f383a5230b99461df2090dfd9c01582f
vahidsaneei <vsdesire@gmail.com>
2019-03-29 00:42:29 +0430

    like service and dao impl
    implements new method bodies

--- SampleStore/src/main/java/com/softup/store/dao/LikeDaoImpl.java
+++ SampleStore/src/main/java/com/softup/store/dao/LikeDaoImpl.java
@@ -1,5 +1,7 @@
 package com.softup.store.dao;
 
+import java.util.List;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.softup.store.entity.Likes;
+import com.softup.store.entity.Product;
+import com.softup.store.entity.User;
 import com.softup.store.interfaces.LikeDao;
 
 @Repository("likeDao")
@@ -44,13 +48,31 @@ public class LikeDaoImpl implements LikeDao {
 
 		try {
 			session().remove(like);
-			result = "success";
 			session().flush();
-		} catch (Exception e) {
-			result = "Erro :some error occurred";
+			result = "success";
+		} catch (HibernateException e) {
+			result = "Erro :some error occurred" + e.getMessage();
+
 		} finally {
 			session().clear();
 		}
 		return result;
 	}
+
+	public boolean likedByUser(User user, Product product) {
+		Likes like = null;
+		String query = "From Likes where user =:user AND product =:product";
+		List<Likes> likes = session().createQuery(query, Likes.class).setParameter("user", user)
+				.setParameter("product", product).list();
+
+		if (likes.size() > 0) {
+			like = likes.get(0);
+		}
+
+		if (like != null)
+			return true;
+
+		return false;
+	}
+
 }
--- SampleStore/src/main/java/com/softup/store/service/LikeServiceImpl.java
+++ SampleStore/src/main/java/com/softup/store/service/LikeServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softup.store.entity.Likes;
+import com.softup.store.entity.Product;
+import com.softup.store.entity.User;
 import com.softup.store.interfaces.LikeDao;
 import com.softup.store.interfaces.LikeService;
 
@@ -15,7 +17,7 @@ public class LikeServiceImpl implements LikeService {
 	@Autowired
 	private LikeDao likeDao;
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public String setLike(Likes likes) {
 		return likeDao.setLike(likes);
 	}
@@ -24,5 +26,11 @@ public class LikeServiceImpl implements LikeService {
 	public String unLike(Likes like) {
 		return likeDao.unLike(like);
 	}
+
+	public boolean likedByUser(User user, Product product) {
+		return likeDao.likedByUser(user,product);
+	}
+	
+	
 
 }
