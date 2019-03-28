b2b8d5330e6e0b358c22cbd62a91fcc9d801167b
vahidsaneei <vsdesire@gmail.com>
2019-03-29 00:51:40 +0430

    orderscontroller
    add requestmapping for find orders by id

--- SampleStore/src/main/java/com/softup/store/controller/OrdersController.java
+++ SampleStore/src/main/java/com/softup/store/controller/OrdersController.java
@@ -1,15 +1,19 @@
 package com.softup.store.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.softup.store.entity.CartItem;
 import com.softup.store.entity.Orders;
+import com.softup.store.entity.User;
 import com.softup.store.interfaces.OrderService;
 
 @Controller
@@ -25,6 +29,18 @@ public class OrdersController {
 		ModelAndView model = new ModelAndView("orderslist");
 		List<Orders> orders = orderService.getAllOrders();
 		model.addObject("orders", orders);
+		return model;
+	}
+
+	@RequestMapping(value = "getorder/{id}", method = RequestMethod.GET)
+	public ModelAndView getOrderById(@PathVariable("id") Long id) {
+		ModelAndView model = new ModelAndView("orderinfo");
+		Orders orders = orderService.findById(id);
+		User user = orders.getUser();
+		List<CartItem> items = new ArrayList<CartItem>(orders.getItems());
+		model.addObject("order", orders);
+		model.addObject("customer", user);
+		model.addObject("items", items);
 		return model;
 	}
 }
